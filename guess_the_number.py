07f5a47514c714609ef093b3c1aebd241f73f62f
Ryugazib <ryudanjuna@gmail.com>
2026-09-21 16:35:45 +0700

    Update code

--- guess_the_number.py
+++ guess_the_number.py
@@ -10,9 +10,21 @@ print ('********************************************')
 nama_user = input ('MASUKKAN NAMA: ')
 print (f'HAI {nama_user} SELAMAT DATANG')
 
+percobaan = 3
 tebakan_angka = int(input ('SILAHKAN TEBAK ANGKA 1-10: '))
-while tebakan_angka != angka:
-    print ('MAAF ANDA SALAH TEBAK SILAHKAN TEBAK LAGI: ')
-    tebakan_angka = int(input ('SILAHKAN TEBAK LAGI: '))
 
-print (f'SELAMAT KAMU BENAR ANGKANYA {angka} DAN TEBAKANMU {tebakan_angka}')
+while percobaan > 0 and tebakan_angka != angka: 
+    percobaan -= 1
+
+    if tebakan_angka < angka:
+        print(f'MAAF ANDA SALAH ANGKA TERSEBUH LEBIH TINGGI [ANDA MASIH PUNYA {percobaan} PERCOBAAN LAGI] ')
+    elif tebakan_angka > angka:
+        print (f'MAAF ANDA SALAH ANGKA TERSEBUT LEBIH RENDAH [ANDA MASIH PUNYA {percobaan} PERCOBAAN LAGI] ')
+ 
+    if percobaan > 0:
+        tebakan_angka = int(input ('SILAHKAN TEBAK ANGKA 1-10: '))
+
+if tebakan_angka == angka:
+    print (f'SELAMAT ANDA BENAR ANGKANYA ADALAH {angka} DAN KAMU MENJAWAB {tebakan_angka}')
+else:
+    print (f'MAAF ANDA KALAH ANGKANYA ADALAH {angka} DAN KAMU MENEBAK {tebakan_angka}')
